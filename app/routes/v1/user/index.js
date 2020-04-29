edc96cbfcc49619a4fcf79cb6382f185018249c0
WhyK <windchimeyk@gmail.com>
2020-04-29 21:20:47 +0900

    update: nameの重複を防ぐ処理を追加

--- app/routes/v1/user/index.js
+++ app/routes/v1/user/index.js
@@ -12,26 +12,43 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   if (!req.headers['x-api-key']) {
-    res.end('API KEYがありません')
-    return
+    return res.status(400).json({
+      error: {
+        message: 'API KEYが設定されていません',
+        tip: 'headersにX-API-KEYという名前で設定してください'
+      }
+    })
   }
 
-  const user = {
-    name: req.body.name,
-    screen_name: req.body.screen_name,
-    bio: req.body.bio
-  }
+  db.find({ name: req.body.name }, (err, doc) => {
+    if (err) return res.end(err)
+    if (doc.length) return res.status(409).json({
+      error: {
+        message: '重複したnameがあるため、登録できませんでした',
+        tip: 'nameを確認し、改めて送信してみてください'
+      }
+    })
 
-  db.insert(user, (err, newDoc) => {
-    if (err) res.end(err)
-    else res.json(newDoc)
+    const user = {
+      name: req.body.name,
+      screen_name: req.body.screen_name,
+      bio: req.body.bio
+    }
+    db.insert(user, (err, newDoc) => {
+      if (err) res.end(err)
+      else res.json(newDoc)
+    })
   })
 })
 
 router.delete('/', (req, res) => {
   if (!req.headers['x-api-key']) {
-    res.end('API KEYがありません')
-    return
+    return res.status(400).json({
+      error: {
+        message: 'API KEYが設定されていません',
+        tip: 'headersにX-API-KEYという名前で設定してください'
+      }
+    })
   }
 
   db.remove({ name: req.query.name }, (err, _) => {
